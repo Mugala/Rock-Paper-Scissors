e413e7df4847bb99242e61f73e327d66f2daa121
Angela <awambuikanyi@gmail.com>
2018-03-07 10:53:44 +0300

    image structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var imageSlider = document.getElementById('imagesSlider');
 
-var images = ["url('images/wonder.jpg')", "url('images/all.jpg')", "url('images/paper.jpg')", "url('images/all3.jpeg')", "url('images/paper.jpg')", "url('images/knight.jpeg')"];
+var images = ["url('images/many.jpg')", "url('images/scissors.jpg')", "url('images/paper.jpg')", "url('images/all.jpg')", "url('images/paper.jpg')"];
 
 
 var counter = 0;
