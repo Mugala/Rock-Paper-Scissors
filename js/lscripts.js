162c567e619ccedc0d505f8c7011707a8a874992
mugala <mugalalevi21@gmail.com>
2018-03-09 09:51:45 +0300

    Returns the alert function This commit will return the return the alert function

--- js/lscripts.js
+++ js/lscripts.js
@@ -1,24 +1,48 @@
-  $(".Glad").click(function() {
-      alert("WELCOME!!!" + " " + "THE GLADIAROTR");
-    });
-
-    $("#s-option").click(function() {
-      alert("WELCOME!!!" + " " + "THE WHITE WALKER");
-    });
-
-    $("h3").click(function() {
-        alert("WELCOME!!!" + " " + "THE WHITE WALKER");
-      });
-
-    $("#t-option").click(function() {
-        alert("WELCOME!!!" + " " + "THE DARK KNIGHT");
-      });
-      
+    function Name(name) {
+        this.name=name;
+    }
     
+    Name.prototype.player1=function(){
+        return this.name + " " 
+    }
+         
+    
+    
+    $(document).ready(function() {
+        var inputtedName = $("input#new-first-name").val();
+        var newName = new Name (inputtedName);
 
-    $("#p-option").click(function() {
-      alert("WELCOME!!!" + " " + "THE PIRATE");
-    });
-
-
+    console.log(newName)
+    
+    $(".Glad").click(function() {
+        var inputtedName = $("input#new-first-name").val();
+        var newName = new Name (inputtedName);
+        alert("WELCOME!!! " + newName.player1   () + " THE GLADIATOR");
+      });
   
+      $("#s-option").click(function() {
+        var inputtedName = $("input#new-first-name").val();
+        var newName = new Name (inputtedName);
+        alert("WELCOME!!!" + newName.player1   () + "THE WHITE WALKER");
+      });
+  
+  
+      $("#t-option").click(function() {
+        var inputtedName = $("input#new-first-name").val();
+        var newName = new Name (inputtedName);
+          alert("WELCOME!!!" + newName.player1   () + "THE DARK KNIGHT");
+        });
+        
+      
+  
+      $("#p-option").click(function() {
+        var inputtedName = $("input#new-first-name").val();
+        var newName = new Name (inputtedName);
+        alert("WELCOME!!!" + newName.player1   () + "THE PIRATE");
+      }); 
+    
+    
+      });
+
+
+    
