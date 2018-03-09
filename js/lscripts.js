87d5efd3b5dec8758427cfce0338dc03199293a2
mugala <mugalalevi21@gmail.com>
2018-03-09 08:20:10 +0300

    Changes in the lscripts

---
+++ js/lscripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("f-option").click(function() {
+      alert("WELCOME!!!" + " " + "THE GLADIAROTR");
+    });
+
+    $("s-option").click(function() {
+      alert("WELCOME!!!" + " " + "THE WHITE WALKER");
+    });
+
+    $("t-option").click(function() {
+        alert("WELCOME!!!" + " " + "THE DARK KNIGHT");
+      });
+      
+    
+
+    $("p-option").click(function() {
+      alert("WELCOME!!!" + " " + "THE PIRATE");
+    });
+  });
